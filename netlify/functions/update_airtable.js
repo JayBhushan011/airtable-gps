84a9f4ed4b1c537c736e112b6aa2bbf0c1b1e75a
Jay Bhushan <jaybhushan011@gmail.com>
2025-02-25 00:37:16 +0530

    new commit

---
+++ netlify/functions/update_airtable.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const { recordId, lat, lng } = event.queryStringParameters;
+
+    try {
+        const response = await fetch(
+            `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_NAME}/${recordId}`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    fields: {
+                        "Latitude": lat,
+                        "Longitude": lng
+                    }
+                })
+            }
+        );
+
+        return {
+            statusCode: response.status,
+            body: await response.text()
+        };
+    } catch (error) {
+        return { statusCode: 500, body: error.message };
+    }
+};
