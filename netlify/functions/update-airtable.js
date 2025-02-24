4ce28937b38537c5a97ac3ddd0b095d175e46ad1
Jay Bhushan <jaybhushan011@gmail.com>
2025-02-25 02:35:30 +0530

    seems to have worked

--- netlify/functions/update-airtable.js
+++ netlify/functions/update-airtable.js
@@ -1,14 +1,8 @@
 const fetch = require('node-fetch');
 
-process.env.AIRTABLE_BASE_ID = 'appJsKmmxuFPkRQD3';
-process.env.AIRTABLE_TABLE_NAME = 'tblgSASr9aRL0WGJb';
-const recordId = 'receWoHK50M5lhSvh';
-process.env.AIRTABLE_API_KEY = 'patsScoQySipTIGUG.4c4679e99390113cea4517b12c68726c4a658278a4aea051b916860b943c7737';
-
-
 exports.handler = async (event) => {
     const { recordId, lat, lng } = event.queryStringParameters;
-    
+
     if (!recordId || !lat || !lng) {
         return {
           statusCode: 400,
