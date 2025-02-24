8952c2e28a5ee3a68cf5dc18719ed592241fd4d6
Jay Bhushan <jaybhushan011@gmail.com>
2025-02-25 01:51:14 +0530

    bug fix5

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -5,31 +5,3 @@ process.env.AIRTABLE_TABLE_NAME = 'tblgSASr9aRL0WGJb';
 const recordId = 'receWoHK50M5lhSvh';
 process.env.AIRTABLE_API_KEY = 'patsScoQySipTIGUG.4c4679e99390113cea4517b12c68726c4a658278a4aea051b916860b943c7737';
 
-(async () => {
-    try {
-        const response = await fetch(
-            `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_NAME}/${recordId}`,
-            {
-                method: 'PATCH',
-                headers: {
-                    'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "fields": {
-                        "Latitude": 7,
-                        "Longitude": 8
-                    }
-                })
-            }
-        );
-
-        const responseBody = await response.text();
-        console.log({
-            statusCode: response.status,
-            body: responseBody
-        });
-    } catch (error) {
-        console.error('Error:', error);
-    }
-})();
